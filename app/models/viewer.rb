85ba641b1094580107b3c74c8b7b115b475fb1b0
github username <github email address>
2020-05-19 11:04:34 -0400

    deliverables done (elisheva)

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,30 @@ class Viewer
   def self.all
     @@all
   end
+
+  def reviews
+    Review.all.select{|review| review.viewer == self}
+  end
+
+  def reviewed_movies
+    self.reviews.map{|review| review.movie}
+  end
+
+  def reviewed_movie?(movie)
+    #check through the viewer's reviewed_movies array
+    #to see if any of the movies == movie param
+    self.reviewed_movies.include?(movie)
+  end
+
+  def rate_movie(movie, rating)
+    if !reviewed_movie?(movie)
+      Review.new(self, movie, rating)
+    else
+      # find the review instance
+      review_inst = self.reviews.find{|review| review.movie == movie}
+      # update the rating
+      review_inst.rating = rating
+    end
+  end
   
 end
--- tools/console.rb
+++ tools/console.rb
@@ -9,9 +9,29 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+m1 = Movie.new("Harry Potter")
+m2 = Movie.new("LOTR")
+m3 = Movie.new("The Princess Bride")
+m4 = Movie.new("Inception")
+m5 = Movie.new("Back to the Future")
 
 
+v1 = Viewer.new("Elisheva")
+v2 = Viewer.new("Thomas")
+v3 = Viewer.new("Bob")
 
+
+r1 = Review.new(v1, m1, 7)
+r2 = Review.new(v2, m3, 8.6)
+r3 = Review.new(v2, m2, 8)
+r4 = Review.new(v3, m2, 7.8)
+
+r5 = Review.new(v1, m4, 2)
+r6 = Review.new(v2, m4, 5)
+r7 = Review.new(v3, m4, 8)
+r8 = Review.new(v3, m5, 6)
+r9 = Review.new(v1, m5, 7.8)
+r10 = Review.new(v2, m5, 3)
 
 
 # ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
