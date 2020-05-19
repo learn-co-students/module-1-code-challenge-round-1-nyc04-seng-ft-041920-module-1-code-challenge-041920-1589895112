efe979a27541e5f8d5e1367a007fa05055a15291
jukebox101 <leonora.squires@gmail.com>
2020-05-19 11:12:54 -0400

    deliverables done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,23 @@ class Movie
     @@all
   end
 
+    def reviews
+      Review.all.select {|review| review if review.movie == self}
+    end
+
+    def reviewers
+      reviews.map {|review| review.viewer}
+    end
+
+    def average_rating
+      total_array = reviews.map {|review| review.rating}
+      total = total_array.sum
+      total = total / total_array.length
+      total
+    end
+
+    def self.highest_rated
+      max = 0
+      Movie.all.max {|movie| movie.average_rating}
+    end
 end
