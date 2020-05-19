b74eb0fb1393e0f73b2886d80ffc633eb7af7ab4
residenttrickster <metkusblackus@gmail.com>
2020-05-19 11:17:17 -0400

    deliverables done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,15 +1,49 @@
 class Movie
+
   attr_accessor :title
 
   @@all = []
 
   def initialize(title)
     @title = title
+    
     self.class.all << self
+    #@@all << self
   end
 
   def self.all
     @@all
   end
+
+  def title
+    return "#{title}"
+  end 
+
+  def reviews
+    #returns an array of all the `Review` instances for the `Movie`
+    Review.all.select do |review|
+      review.movie == self
+    end 
+  end 
+
+  def reviewers
+    #returns an array of all of the `Viewer` instances that reviewed the `Movie
+    self.reviews.map do |movie|
+      movie.viewer
+    end 
+  end 
+
+  def average_rating
+   average = @@all.rating.sum do |movie|
+      movie.rating / self.rating.length 
+   end 
+   return average 
+  end 
+
+  def self.highest_rated
+    average_rating.max_by do |movie|
+      movie.rating 
+    end 
+  end 
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -9,7 +9,19 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+def reviews
+  #returns an array of all the `Review` instances for the `Movie`
+  Review.all.select do |review|
+    review.movie == self
+  end 
+end 
 
+def reviewers
+  #returns an array of all of the `Viewer` instances that reviewed the `Movie
+  self.reviews.map do |movie|
+    movie.viewer
+  end 
+end 
 
 
 
