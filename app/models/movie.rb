6b22995c86a227907e07e271722644cfd9b5e6d3
github username <github email address>
2020-05-19 11:29:06 -0400

    Done.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,21 @@ class Movie
     @@all
   end
 
+  # - `Movie#title`
+  # - returns the `Movie`'s title
+
+def title 
+  movie.map do |title|
+    return title.self 
+end 
+
+# - `Movie.all`
+#   - returns an array of all the `Movie` instances that have been initialized
+
+def self.movies 
+  self.all.select do |movies|
+    movies.all == self 
+  end 
+end 
+
 end
