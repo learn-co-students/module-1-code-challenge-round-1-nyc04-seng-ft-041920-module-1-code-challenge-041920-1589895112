9f3c1b7fce0902c0d3bf1ea1c0843af361fa8e85
fpolanco <polanco.franchell@gmail.com>
2020-05-19 11:35:55 -0400

    franchell challenge

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -7,6 +7,25 @@ class Movie
     @title = title
     self.class.all << self
   end
+ 
+  def reviews
+    Review.all.select do |review|
+      review.movie == self
+   end
+  end
+
+  def reviewers
+    Viewer.all.select do |viewer|
+      viewer.movie == self
+   end
+  end
+
+  def average_rating
+# returns the average of all ratings for the Movie instance
+# to average ratings, add all ratings together and divide 
+# by the total number of ratings. access ratings from review?
+ratings.sum / ratings.length
+  end
 
   def self.all
     @@all
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,22 @@
 class Review
 
+attr_accessor :viewer, :movie, :rating
 
+    def initialize(viewer, movie, rating)
+         @viewer = viewer   
+         @movie = movie
+         @rating = rating
+    end
+
+    def self.all
+        @@all
+      end
+
+    def viewer
+        self.viewer
+    end
+
+    def movie
+        self.movie
+    end
 end
