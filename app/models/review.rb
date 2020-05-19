e1740c4782baf3c01cae67da3ec165612cf3404f
github username <github email address>
2020-05-19 11:34:05 -0400

    code for code challenge

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,33 @@
 class Review
+  attr_reader :viewer, :movie, :rating
 
+  @@all = []
 
+  def save
+    @@all << self
+  end
+
+  def initialize(viewer, movie, rating)
+    @viewer = viewer
+    @movie = movie
+    @rating = rating
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def viewer_v
+    Viewer.all.select do |vw|
+      vw.username == self.viewer.username
+    end
+  end
+
+  def movie_v
+    Movie.all.select do |mv|
+      #   binding.pry
+      self.movie.title == mv.title
+    end
+  end
 end
