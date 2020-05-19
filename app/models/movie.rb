59f195951cfb2dd1cb0f8c23fb47295696b522ff
amer519 <afahmy519@gmail.com>
2020-05-19 11:33:21 -0400

    code

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,25 @@ class Movie
     @@all
   end
 
+  def reviews
+    Review.all.select {|review| review.movie == self}
+  end
+
+  def reviewers
+    review.map(&:viewers)
+  end
+
+  def average_rated
+    rating = 0
+    all_reviews = review.map {|reviews| rating += reviews.rating}
+  end
+   rating /= all_reviews.count
+  end
+
+  def self.highest_rated
+    the_reviews = Review.all.map do |review|
+      review 
+      the_reviews.sort_by {|review| review.rating.last}
+  end
+
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,33 @@ class Viewer
   def self.all
     @@all
   end
+
+  def reviews
+    Review.all.select {|review| review.viewer == self}
+  end
+
+  def reviewed_movies
+    reviews.map(&:movie)
+  end
+
+  def reviewed_movie?(movie)
+    review.select {|reviewed| reviewed.viewer == movie}
+     if reviewed.viewer == movie
+      return true
+     else
+      false
+     end
+  end
+
+  def rate_movie(movie, rating)
+    if review.map do |review|
+      review.movie == movie
+      review.rating = rating
+    else
+      Review.new(movie, self, viewer, rating)
+    end
+ end
+
+  
   
 end
